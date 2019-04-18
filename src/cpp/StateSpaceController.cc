731067b99d3028f7f2e864722c005c5d3aa43c8e
Epsilon10 <benjamin.oommen1@gmail.com>
2019-04-18 16:10:24 -0500

    more docs

--- src/cpp/StateSpaceController.cc
+++ src/cpp/StateSpaceController.cc
@@ -1,1 +1,27 @@
 #include "StateSpaceController.hh"
+
+template <uint32_t States, uint32_t Inputs, uint32_t Outputs>
+StateSpaceController<States, Inputs, Outputs>::StateSpaceController(
+    const StateSpaceControllerCoeffs<States, Inputs, Outputs>& controllerCoeffs,
+    StateSpacePlant<States, Inputs, Outputs>& plant)
+    : plant(&plant) {
+  AddCoefficients(controllerCoeffs);
+}
+
+template <uint32_t States, uint32_t Inputs, uint32_t Outputs>
+void StateSpaceController<States, Inputs, Outputs>::enable() {
+    _enabled = true;
+}
+
+template <uint32_t States, uint32_t Inputs, uint32_t Outputs>
+void StateSpaceController<States, Inputs, Outputs>::enable() {
+    _enabled = false;
+}
+
+template <uint32_t States, uint32_t Inputs, uint32_t Outputs>
+const Eigen::Matrix<double, Inputs, States>& 
+StateSpaceController<States, Inputs, Outputs>::Kff(uint32_t row, uint32_t column) const {
+    
+}
+
+
--- src/include/StateSpaceController.hh
+++ src/include/StateSpaceController.hh
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "SpaceOut.hh"
+#include "StateSpacePlant.hh"
 
 template <uint32_t States, uint32_t Inputs, uint32_t Ouputs>
 struct StateSpaceControllerCoeffs { 
@@ -27,21 +28,18 @@ class StateSpaceController {
          * 
          * @param controllerCoeffs Controller Coeffecients
          * @param plant
-         */
-
+         */]
         StateSpaceController(const StateSpaceControllerCoeffs<States, Inputs, Outputs>& controllerCoeffs, 
             StateSpacePlant<States, Inputs, Outputs>& plant);
         
         /**
          * Enables controller.
          */
-
         void enable();
 
         /**
          * Disables controller, resets the output to U
          */ 
-
         void disable();
 
         /**
@@ -50,7 +48,70 @@ class StateSpaceController {
          * @param row Row of K.
          * @param column Column of K.
          */
-        
         double K(uint32_t row, uint32_t column) const;
+
+        /**
+         * Returns the feedforward matrix Kff
+         */
+        const Eigen::Matrix<double, Inputs, States>& Kff() const;
+
+        /**
+         * Returns an element from the feedforward matrix Kff 
+         * 
+         * @param row Row of Kff
+         * @param column Column of Kff
+         */ 
+
+        const Eigen::Matrix<double, Inputs, States>& Kff(uint32_t row, uint32_t column) const;
+
+        /**
+         * Returns reference vector r
+         */ 
+        const Eigen::Matrix<double, States, 1>& R() const;
+
+        /**
+         * Returns an element of the reference vector r.
+         *
+         * @param row Row of r.
+         */
+        double R(uint32_t row) const;
+
+        /**
+         * Resets the controller
+         */
+        void reset();
+
+        /**
+         * Update the controller without setting a new reference
+         * 
+         * @param x the current state x
+         */
+        void update(const Eigen::Matrix<double, States, 1>& x);
+
+        /**
+         * Update the controller and set a new reference
+         * 
+         * @param x the current state x
+         * @param nextR the next reference vector
+         */
+        void update(const Eigen::Matrix<double, States, 1>& x, const Eigen::Matrix<double, States, 1>& nextR);
+
+
+
+    private:
+        StateSpacePlant<States, Inputs, Outputs>* plant;
+
+        bool _enabled = false;
+
+        // Current reference
+        Eigen::Matrix<double, States, 1> _r;
+
+        // Capped computed controller output
+        Eigen::Matrix<double, Inputs, 1> _u;
+
+        std::vector<StateSpaceControllerCoeffs<States, Inputs, Outputs>> _ceoffecients;
+        uint32_t index = 0;
+        
+        void cap_u();
 
 };
